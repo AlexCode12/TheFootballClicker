3ba3a2a38ab307b15feac0ce3968f67a6efc4b4e
AlexCode12 <alexander.olsson2@elev.ga.ntig.se>
2019-04-25 15:14:12 +0200

    250419. Skriptet gör nu spelet spelbart

--- js/skript.js
+++ js/skript.js
@@ -9,41 +9,52 @@ let clickValue = 1; // vad är varje click värt
 let bank = 0;
 let pupCost1 = 10;
 let pupCost2 = 15;
+let pup1Clicks = 0;
 let refClock = 0;
 
 /* lyssna på knappen efter click event */
 gameButton.addEventListener('click', function() {
-    bank += 1;
+    bank += clickValue;
     gameButton.textContent = Math.floor(bank);
 });
 /* skapa ett powerup-element */
 let powerUp = document.createElement("button");
 powerUp.textContent = "THE FOOTBALL " + pupCost1;
+
 powerUp.addEventListener('click', function() {
     if (bank >= pupCost1) {
-        bank = bank - pupCost1;
-        pupCost1 = pupCost1 * 2;
-        bank = bank * 2;
-        gameButton.textContent = bank;
-        powerUp.textContent = "The power of Greyskull! " + pupCost1;
-        bankElement.textContent = "Du köpte powerup!";
-    } else
-        bankElement.textContent = "Du har inte råd med powerup!";
+		clickValue *= 2;
+		bank -= pupCost1;
+        pupCost1 *= 3;
+        pup1Clicks += 10;
+        gameButton.textContent = bank; 
+       bankElement.textContent = "Du köpte THE FOOTBALL!"
+       powerUp.textContent = "THE FOOTBALL " + pupCost1;
+    } else {
+        bankElement.textContent = "Du har inte råd med THE FOOTBALL!";
+    }
+    if (bank >= 1000000) {
+        bankElement.textContent = "LEEDS LEEDS LEEDS";
+    }
 });
 powerUpsDiv.appendChild(powerUp);
 
 /* skapa ett powerup-element */
 let powerUrp = document.createElement("button");
-powerUrp.textContent = "THE TEAM " + pupCost2;
+powerUrp.textContent = "THE CLUB " + pupCost2;
 powerUrp.addEventListener('click', function() {
     if (bank >= pupCost2) {
         bank = bank - pupCost2;
-        pupCost2 = pupCost2 * 3;
+        pupCost2 *= 2;
         bank = bank * 2;
         gameButton.textContent = bank;
-        powerUrp.textContent = "PowerURP! " + pupCost;
-        bankElement.textContent = "Du köpte powerurp!";
+        bankElement.textContent = "Du köpte THE TEAM!";
+        powerUrp.textContent = "THE TEAM " + pupCost2;
     } else
-        bankElement.textContent = "Du har inte råd med powerurp!";
+        bankElement.textContent = "Du har inte råd med THE TEAM!";
+        if (bank >= 1000000) {
+            bankElement.textContent = "LEEDS LEEDS LEEDS";
+        }
 });
+
 powerUpsDiv.appendChild(powerUrp);
